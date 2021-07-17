7e690a86e99011bbd4c4e41e1e155195dac1db51
Daniel Strunk <daniel@strunk.me>
2021-07-17 12:45:39 -0400

    Convert uiComponents to using `tracks`
    - Instead of using ko explicitly, use Magento's uiComponent `tracks`
      method for handling observables. For more information, see this Alan
      Storm article:
      <https://alanstorm.com/observables-uielement-objects-and-variable-tracking/>.

--- view/frontend/web/js/product-quiz.js
+++ view/frontend/web/js/product-quiz.js
@@ -35,43 +35,22 @@ define([
                 title: 'Question 2',
                 answers: [
                     {
-                        id: 0,
+                        id: 3,
                         title: 'Answer 1',
                         selected: false,
                     },
                     {
-                        id: 1,
+                        id: 4,
                         title: 'Answer 2',
                         selected: false,
                     },
                     {
-                        id: 2,
+                        id: 5,
                         title: 'Answer 3',
                         selected: false,
                     },
                 ],
-            },
-            {
-                id: 2,
-                title: 'Question 3',
-                answers: [
-                    {
-                        id: 0,
-                        title: 'Answer 1',
-                        selected: false,
-                    },
-                    {
-                        id: 1,
-                        title: 'Answer 2',
-                        selected: false,
-                    },
-                    {
-                        id: 2,
-                        title: 'Answer 3',
-                        selected: false,
-                    },
-                ],
-            },
+            }
         ],
     };
 
@@ -79,62 +58,63 @@ define([
     return Component.extend({
         defaults: {
             template: 'Silentpost_ProductQuiz/product-quiz',
+            tracks: {
+                isVisible: true,
+                errorStage: true,
+                introStage: true,
+                quizStage: true,
+                quizDescription: true,
+                questions: true,
+                quizIndex: true,
+            },
         },
-
-        isVisible: ko.observable(false),
-        errorStage: ko.observable(false),
-        introStage: ko.observable(false),
-        quizStage: ko.observable(false),
-        quizTitle: ko.observable(''),
-        quizDescription: ko.observable(''),
-        questions: [],
-        quizIndex: ko.observable(0),
 
         initialize: function () {
             self = this
             self._super()
 
+            self.questions = []
+
             self.fetchQuiz()
         },
 
         openQuiz: function () {
-            this.isVisible(true)
+            self.isVisible = true
         },
 
         closeQuiz: function () {
-            this.isVisible(false)
+            self.isVisible = false
         },
 
         startQuiz: function () {
-            self.introStage(false)
-            self.quizStage(true)
+            self.introStage = false
+            self.quizStage = true
         },
 
         previous: function () {
-            self.quizIndex(self.quizIndex() - 1)
+            self.quizIndex = self.quizIndex - 1
         },
 
         next: function () {
-            self.quizIndex(self.quizIndex() + 1)
+            self.quizIndex = self.quizIndex + 1
         },
 
         fetchQuiz: function () {
             // @TODO: Request quiz information and questions from controller.
             // @TODO: Push questions into KO observable array.
-            self.quizTitle(data.title)
-            self.quizDescription(data.description)
+            self.quizTitle = data.title
+            self.quizDescription = data.description
             data.questions.forEach(function (question) {
-                let koComponent = new QuizQuestion(question)
-                self.questions.push(koComponent)
+                self.questions.push(new QuizQuestion(question))
             })
 
-            self.quizIndex(0)
+            self.quizIndex = 0
 
             self.currentQuestion = ko.pureComputed(function () {
-                return self.questions[self.quizIndex()]
+                return self.questions[self.quizIndex]
             }, self)
 
-            self.introStage(true)
+            self.introStage = true
         },
 
         submit: function () {
