2f0bf9bf92a9347787212e70d4409099b9365a35
Daniel Strunk <daniel@strunk.me>
2021-10-18 20:25:28 -0400

    Mock out quiz results post response
    - For now, set the results post request to return JSON expected for the
      product quiz, containing a response and a redirect URL

--- Controller/Results/Post.php
+++ Controller/Results/Post.php
@@ -3,13 +3,39 @@
 namespace Silentpost\ProductQuiz\Controller\Results;
 
 use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\UrlInterface;
 
 class Post implements HttpPostActionInterface
 {
+    /** @var RequestInterface */
+    private $request;
+
+    /** @var UrlInterface */
+    private $url;
+
+    /** @var JsonFactory */
+    private $jsonFactory;
+
+    public function __construct(
+        RequestInterface $request,
+        UrlInterface $url,
+        JsonFactory $jsonFactory
+    )  {
+        $this->request = $request;
+        $this->url = $url;
+        $this->jsonFactory = $jsonFactory;
+    }
+
     /**
      * @inheritDoc
      */
     public function execute()
     {
+        return $this->jsonFactory->create()->setData([
+            'response' => 'success',
+            'redirect_url' => $this->url->getUrl('*/*/index'),
+        ]);
     }
 }
